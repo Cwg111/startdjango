09ab0c8ee9b73fc1062210c20250fb0659b71136
chenwengang <chenwengang@cppe.com.cn>
2025-11-03 16:01:44 +0800

    清理掉不需要提交到git中的本地配置文件和数据库数据

---
+++ frontend/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :test.py
+# @Time      :2025/11/3 15:48
+# @Author    :ChenWenGang
+print("这是一个前端目录的占位文件")
+
+if __name__ == "__main__":
+    run_code = 0
