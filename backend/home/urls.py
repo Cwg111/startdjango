3c670b890759278c7609d4f8596cadefaf5aebf4
cwg <3616517775@qq.com>
2025-11-05 00:00:01 +0800

    自己电脑学完模板标签（2）

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -4,16 +4,22 @@
 # @Time      :2025/11/4 15:13
 # @Author    :ChenWenGang
 from django.urls import path
-
-from movie.urls import app_name
 from . import views
 
+app_name = "home"
 urlpatterns = [
-    path('', views.index, name='home_index'),
-    path('baidu', views.index_baidu, name='home_baidu'),
-    path('info',views.info,name='home_info')
-
-
+    path("", views.index, name="home_index"),
+    path("baidu", views.index_baidu, name="home_baidu"),
+    path("info", views.info, name="home_info"),
+    path("if", views.if_demo, name="home_if"),
+    path("for", views.for_demo, name="home_for"),
+    path("with", views.with_demo, name="home_with"),
+    path("url", views.url_demo, name="home_url"),
+    path("book/<int:book_id>", views.book_demo, name="home_book"),
+    path("book", views.book_demo_query_string, name="home_book_query_string"),
+    path(
+        "book/<int:user_id>/<str:user_name>", views.book_demo_mix, name="home_book_mix"
+    ),
 ]
 
 if __name__ == "__main__":
--- backend/home/views.py
+++ backend/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
@@ -34,3 +34,55 @@ def info(request):
         "person": Person("小王", 18),
     }
     return render(request, "info.html", context=context)
+
+
+def if_demo(request):
+    age = 20
+    return render(request, "if.html", context={"age": age})
+
+
+def for_demo(request):
+    # 列表
+    books = [
+        {"name": "水浒传", "author": "施耐庵"},
+        {"name": "三国演义", "author": "罗贯中"},
+    ]
+    # 字典
+    person = {"name": "小王", "age": 18, "height": 180}
+    # 空的列表，空循环
+    empty_list = []
+    context = {"books": books, "person": person, "empty_list": empty_list}
+    return render(request, "for.html", context=context)
+
+
+def with_demo(request):
+    return render(
+        request,
+        "with.html",
+        context={"person": {"name": "小王", "age": 18, "height": 180}},
+    )
+
+
+def url_demo(request):
+    return render(request, "url.html", context={})
+
+
+#  path带参数的url
+def book_demo(request, book_id):
+    return HttpResponse(f"图书详情id为：{book_id}")
+
+
+#  query string带参数的url
+def book_demo_query_string(request):
+    book_id = request.GET.get("id")
+    book_name = request.GET.get("name")
+    return HttpResponse(f"图书id为：{book_id},图书名称为：{book_name}")
+
+
+# 多path带参数和 query string带参数的混合的 url
+def book_demo_mix(request, user_id, user_name):
+    book_id = request.GET.get("id")
+    book_name = request.GET.get("name")
+    return HttpResponse(
+        f"用户id为：{user_id},用户名称为：{user_name},图书id为：{book_id},图书名称为：{book_name}"
+    )
